06d0b8d29b770f14ad270ac23824bad32bc0eca3
nsobczak <nicolas.sobczak@orange.fr>
2017-08-16 21:01:02 +0200

    Add other pages

--- ressources/navBar.js
+++ ressources/navBar.js
@@ -1,6 +1,6 @@
 document.write('\
 <nav class="navbar navbar-expand-lg navbar-default navbar-fixed-top navbar-fnt navbar-backgrnd">\
-    <div class="container">\
+    <div class="container-fluid">\
 \
         <div class="navbar-header">\
             <button class="navbar-toggle" type="button" data-toggle="collapse"\
@@ -17,17 +17,17 @@ document.write('\
             <ul class="nav navbar-nav mr-auto">\
 \
                 <li class="nav-item active">\
-                    <a class="nav-link" href="#">Home (infos)<span class="sr-only">(current)</span></a>\
+                    <a class="nav-link" href="index.html">Home (infos)<span class="sr-only"></span></a>\
                 </li>\
                 <li class="nav-item">\
-                    <a class="nav-link" href="#">Skills</a>\
+                    <a class="nav-link" href="skills.html">Skills</a>\
                 </li>\
                 <li class="nav-item">\
-                    <a class="nav-link" href="#">Projects</a>\
+                    <a class="nav-link" href="projects.html">Projects</a>\
                 </li>\
-                <li class="nav-item">\
-                    <a class="nav-link" href="#">Contact</a>\
-                </li>\
+                <!-- <li class="nav-item"> -->\
+                    <!-- <a class="nav-link" href="#">Contact</a> -->\
+                <!-- </li> -->\
             </ul>\
         </div>\
 \
