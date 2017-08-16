4bab2379fe5a803a0bf2bb80a4e2ba2450e5c54b
nsobczak <nicolas.sobczak@orange.fr>
2017-08-16 19:24:54 +0200

    Move navBar to navBar.js file

---
+++ ressources/navBar.js
@@ -0,0 +1,36 @@
+document.write('\
+<nav class="navbar navbar-expand-lg navbar-default navbar-fixed-top navbar-fnt navbar-backgrnd">\
+    <div class="container">\
+\
+        <div class="navbar-header">\
+            <button class="navbar-toggle" type="button" data-toggle="collapse"\
+                    data-target="#navbarSupportedContent"\
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+            </button>\
+            <a class="navbar-brand">Navbar title</a>\
+        </div>\
+\
+        <div id="navbarSupportedContent" class="navbar-collapse collapse">\
+            <ul class="nav navbar-nav mr-auto">\
+\
+                <li class="nav-item active">\
+                    <a class="nav-link" href="#">Home (infos)<span class="sr-only">(current)</span></a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="#">Skills</a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="#">Projects</a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="#">Contact</a>\
+                </li>\
+            </ul>\
+        </div>\
+\
+    </div>\
+</nav>'
+);
