17684b4608be794198e87a94cacc867a7f331a00
nsobczak <nicolas.sobczak@orange.fr>
2017-09-07 14:03:22 +0200

    Index: change carousel to banner

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,7 @@
 document.write('\
     <div class="footer container-fluid">\
-        <span class="text-muted">Website and layout by Nicolas Sobczak</span>\
+        <span class="text-muted">Website and layout by <a href="https://github.com/nsobczak">Nicolas Sobczak</a></span><br>\
+        <span class="text-muted">Index based on <a href="http://w3layouts.com">W3layouts</a>;</span>\
+        <span class="text-muted">Project thumbnail based on <a href="https://github.com/codrops/ThumbnailGridExpandingPreview">Mary Lou</a></span>\
     </div>'
 );
