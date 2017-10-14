88ecf2a8441fe2f2dedfdfb7fc2737e898a2760d
nsobczak <nicolas.sobczak@orange.fr>
2017-10-14 14:15:43 +0200

    - add icons to links - change footer link

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.write('\
     <div class="footer container-fluid">\
         <span class="text-muted">Website and layout by <a href="https://github.com/nsobczak">Nicolas Sobczak</a></span><br>\
-        <span class="text-muted">Project thumbnail grid based on <a href="https://github.com/codrops/ThumbnailGridExpandingPreview">Mary Lou</a></span>\
+        <span class="text-muted">Project thumbnail grid based on <a href="https://github.com/codrops/ThumbnailGridExpandingPreview">Codrops</a></span>\
     </div>'
 );
